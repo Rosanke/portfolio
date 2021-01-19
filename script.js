30c1642c2d0f969ba7d746f87642d24e03827ab6
Anke <mail@ankestrunz.de>
2021-01-19 17:43:57 +0100

    Javascript added

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const idA ="";
+const idB ="";
+
+function showHide1(idA, idB) {
+    document.getElementById(idA).style.display = "none";
+    document.getElementById(idB).style.display = "block";
+}
+
+function showHide2(idA, idB) {
+    document.getElementById(idA).style.display = "none";
+    document.getElementById(idB).style.display = "flex";
+}
+
+
+
+
+
+
+
+
